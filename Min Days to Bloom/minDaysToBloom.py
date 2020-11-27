b02ddb62b3aba047d01656eb358b930ca9451165
Stephane Meunier <Stephbysteph16@gmail.com>
2020-11-27 17:01:45 -0500

    Solved Min Days To Bloom by watching a concept video

--- Min Days to Bloom/minDaysToBloom.py
+++ Min Days to Bloom/minDaysToBloom.py
@@ -6,30 +6,53 @@ def minDaysToBloom(roses, m , k):
 
     if k*m > len(roses):
             return -1
+    # # Too slow
+    # while bouquets < m:
+    #     bouquets = 0
+    #     running = 0
+    #     days += 1
 
-    while bouquets < m:
+    #     for rose in roses:
+    #         if rose <= days:
+    #             running += 1
+    #             if running == k:
+    #                 bouquets += 1
+    #                 running = 0
+    #         else:
+    #             running = 0
+
+    # Implementing binary search
+    
+    left = min(roses)
+    right = max(roses)
+    optimal = max(roses)
+    
+    while left <= right:
+        days = int((left+right)/2)
         bouquets = 0
         running = 0
-        days += 1
-
+        
         for rose in roses:
             if rose <= days:
                 running += 1
-                # print("rose, running, days:", rose, running, days)
-                # print("bouquets:", bouquets)
                 if running == k:
-                    # print("rose, running, days:", rose, running, days)
-                    # print("bouquets:", bouquets)
                     bouquets += 1
                     running = 0
             else:
                 running = 0
-
-    return days
+        if bouquets == m:
+            if days < optimal:
+                optimal = days
+        if bouquets < m:
+            left = days + 1
+        else:
+            right = days - 1
+    
+    return optimal
 
 
 if __name__ == '__main__':
-    # print(minDaysToBloom([1, 2, 4, 9, 3, 4, 1], 2, 2))
-    # print(minDaysToBloom([1, 10, 3, 10, 2], 3, 2))
+    print(minDaysToBloom([1, 2, 4, 9, 3, 4, 1], 2, 2))
+    print(minDaysToBloom([1, 10, 3, 10, 2], 3, 2))
     print(minDaysToBloom([7,7,7,7,12,7,7], 2 , 3))
     
