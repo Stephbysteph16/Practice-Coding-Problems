d55542070dd4c6ab9168a6115d2efe7bf8e0a8dd
Stephane Meunier <Stephbysteph16@gmail.com>
2020-12-04 18:18:40 -0500

    Solved Maximum Level Sum

--- Maximum Level Sum of a Binary Tree/maximumLevelSum.py
+++ Maximum Level Sum of a Binary Tree/maximumLevelSum.py
@@ -1,34 +1,29 @@
-from collections import defaultdict
-from collections import deque
 # Definition for a binary tree node.
+from collections import deque
+from collections import defaultdict
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-def maxLevelSum(root: TreeNode):
-    
-    r = root
-    queue = deque()
-    lvl = 1
-    queue.append((r,lvl))
-    lvl_sum = defaultdict(lambda:0)
-    
-    while(queue):
-        r, lvl = queue.pop()
-        lvl_sum[lvl] += r.val
-        if r.right != None:
-            queue.append((r.right, lvl+1))
-        if r.left != None:
-            queue.append((r.left,lvl+1))
-    
-    
-    return max(lvl_sum.keys(), key=(lambda k: lvl_sum[k]))
-        
-        
 
+class Solution:
+    def maxLevelSum(self, root: TreeNode) -> int:
+        r = root
+        queue = deque()
+        lvl = 1
+        queue.append((r, lvl))
+        lvl_sum = defaultdict(lambda: 0)
 
-if __name__ == '__main__':
-    print(maxLevelSum([1, 7, 0, 7, -8, None, None]))
-        
+        while(queue):
+            r, lvl = queue.pop()
+            lvl_sum[lvl] += r.val
+            if r.right != None:
+                queue.append((r.right, lvl+1))
+            if r.left != None:
+                queue.append((r.left, lvl+1))
+
+        return max(lvl_sum.keys(), key=(lambda k: lvl_sum[k]))
