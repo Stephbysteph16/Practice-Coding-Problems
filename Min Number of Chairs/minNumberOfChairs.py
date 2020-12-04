0b6be47da11c11665591eca9975edc73fee982ac
Stephane Meunier <Stephbysteph16@gmail.com>
2020-12-04 16:56:02 -0500

    Solved Min Number Of Chairs with O(n*m) where m is the times. Faster Solution?

--- Min Number of Chairs/minNumberOfChairs.py
+++ Min Number of Chairs/minNumberOfChairs.py
@@ -4,7 +4,15 @@ def minNumberOfChairs(S, E):
     n = len(S)
     if n==0:
         return 0
-    attendees = defaultdict(lambda: 0)
+    times = defaultdict(lambda: 0)
     
+    for i in range(len(S)):
+        for time in range(S[i], E[i]):
+            times[time] += 1
     
+    return max(times.values())
+
+if __name__ == '__main__':
+    print(minNumberOfChairs(S=[1, 2, 6, 5, 3], E=[5, 5, 7, 6, 8]))
+    print(minNumberOfChairs(S=[1, 2, 10, 5, 5], E=[4, 5, 12, 9, 12]))
     
